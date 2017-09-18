72ebfa2af42d2c6007f243449ede1e7fdef40ae6
chris <chrismahnchris@aol.com>
2017-09-17 19:25:26 -0500

    created a home function in views and created url for it to give a simple hello http response

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+#function base view
+def home(request):
+    return HttpResponse('hello')
+    #return render(request, 'home.html', {})
