b553cc21057df653a6772ffefe6f562f7363b792
chris <chrismahnchris@aol.com>
2017-09-17 19:47:07 -0500

    created a jinja logic function with random class in views.py home function to display a random value integer

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,18 +1,9 @@
 from django.shortcuts import render
+import random
 from django.http import HttpResponse
 
 # Create your views here.
 #function base view
 def home(request):
-    html_ = """<!DOCTYPE html>
-    <html lang='eng'>
-    <head>
-    </head>
-    <body>
-    <h1>Hello Nurse</h1>
-    <p>This is html syntax</p>
-    </body>
-    </html>
-    """
-    return HttpResponse(html_)
-    #return render(request, 'home.html', {})
+    nums = random.randint(0, 10000)
+    return render(request, 'base.html', {'nums': nums})
