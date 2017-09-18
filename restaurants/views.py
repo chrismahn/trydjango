cad6d0d68ca2d394b2d2272a54d441aeab485e70
chris <chrismahnchris@aol.com>
2017-09-17 19:31:38 -0500

    added html to views.py home function for better message display

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,5 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 #function base view
 def home(request):
-    return HttpResponse('hello')
+    html_ = """<!DOCTYPE html>
+    <html lang='eng'>
+    <head>
+    </head>
+    <body>
+    <h1>Hello Nurse</h1>
+    <p>This is html syntax</p>
+    </body>
+    </html>
+    """
+    return HttpResponse(html_)
     #return render(request, 'home.html', {})
