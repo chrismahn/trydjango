d1515872014742a989d5a8a2a53cccc5bcf5d309
chris <chrismahnchris@aol.com>
2017-09-19 18:00:17 -0500

    created database tables for admin

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
-from . models import Restaurant
 
-admin.site.register(Restaurant)
+# Register your models here.
+from .models import RestaurantLocation
+
+admin.site.register(RestaurantLocation)
