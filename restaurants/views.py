7169228baaa8c4fe3d03f2ab095ab73d3f809b94
chris <chrismahnchris@aol.com>
2017-09-18 20:52:16 -0500

    created template inheritance from base.html to home,home2,home3

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,10 +4,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 #function base view
+
 def home(request):
     nums = random.randint(0, 10000)
     nums2 = random.randint(0, 20000)
     nums3 = random.randint(0, 30000)
     some_list = [nums, nums2, nums3]
     context = {'nums': nums, 'some_list': some_list}
-    return render(request, 'base.html', context)
+    return render(request, 'home.html', context)
+
+def home2(request):
+    nums = random.randint(0, 10000)
+    nums2 = random.randint(0, 20000)
+    nums3 = random.randint(0, 30000)
+    some_list = [nums, nums2, nums3]
+    context = {'nums': nums, 'some_list': some_list}
+    return render(request, 'home2.html', context)
+
+def home3(request):
+    nums = random.randint(0, 10000)
+    nums2 = random.randint(0, 20000)
+    nums3 = random.randint(0, 30000)
+    some_list = [nums, nums2, nums3]
+    context = {'nums': nums, 'some_list': some_list}
+    return render(request, 'home3.html', context)
