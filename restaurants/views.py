39ee9beca1bb0a6ebb5c019ddd02e7bb09ed1267
chris <chrismahnchris@aol.com>
2017-09-19 19:24:37 -0500

    updated templateviews chaged the home view to an html template.

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,43 +1,14 @@
 from django.shortcuts import render
-import random
 from django.views.generic import TemplateView
+from .models import RestaurantLocation
 
 
 # Create your views here.
-#function base view
 
-def home(request):
-    nums = random.randint(0, 10000)
-    nums2 = random.randint(0, 20000)
-    nums3 = random.randint(0, 30000)
-    some_list = [nums, nums2, nums3]
-    context = {'nums': nums, 'some_list': some_list}
-    return render(request, 'home.html', context)
-
-def about(request):
-    context = {}
-    return render(request, 'about.html', context)
-
-def contact(request):
-    context = {}
-    return render(request, 'contact.html', context)
-
-'''
-## create  a class based View ##
-class ContactView(View):
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, 'contact.html', context)
-'''
-
-class HomeView(TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-        nums = random.randint(0, 10000)
-        nums2 = random.randint(0, 20000)
-        nums3 = random.randint(0, 30000)
-        some_list = [nums, nums2, nums3]
-        context = {'nums': nums, 'some_list': some_list}
-        return context
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, template_name, context)
--- trydjango/old_settings.py
+++ trydjango/old_settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'restaurants/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
