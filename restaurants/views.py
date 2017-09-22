9a1ee1ba8ba3976df32bb313bd3a116e2afbe4c5
chris <chrismahnchris@aol.com>
2017-09-21 20:54:35 -0500

    updated views and restaurantlocation_detail template

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,23 +1,23 @@
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import (TemplateView, ListView,
+                                  DetailView)
 from .models import RestaurantLocation
 
 
 # Create your views here.
 
 def restaurant_listview(request):
-    template_name = 'restaurants/restaurants_list.html'
+    template_name = 'restaurants/restaurantlocation_list.html'
     queryset = RestaurantLocation.objects.all()
     context = {
         'object_list': queryset
     }
     return render(request, template_name, context)
 
-class RestaurantListView(ListView):
-    template_name = 'restaurants/restaurants_list.html'
 
+class RestaurantListView(ListView):
     def get_queryset(self):
         slug = self.kwargs.get('slug')
         if slug:
@@ -27,6 +27,20 @@ class RestaurantListView(ListView):
             queryset = RestaurantLocation.objects.all()
         return queryset
 
+
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        print(self.kwargs)
+        context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get('rest_id')
+        obj = get_object_or_404(RestaurantLocation, id=rest_id)
+        return obj
 
 
 
