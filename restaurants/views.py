1016613b8a3dbef660fa92f60a95d75c7227ed02
chris <chrismahnchris@aol.com>
2017-09-21 22:26:19 -0500

    creating forms for adding restaurant and posting to db and redirecting to restaurants list page

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,12 +1,29 @@
 from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.views import View
 from django.views.generic import (TemplateView, ListView,
                                   DetailView)
 from .models import RestaurantLocation
-
+from .forms import RestaurantCreateForm
 
 # Create your views here.
+
+
+def restaurant_createview(request):
+    if request.method == 'POST':
+        title =request.POST.get('title')
+        location = request.POST.get('location')
+        category = request.POST.get('category')
+        obj = RestaurantLocation.objects.create(
+            name = title,
+            location = location,
+            category = category,
+        )
+        return HttpResponseRedirect('/restaurants/')
+    template_name = 'restaurants/form.html'
+    context = {}
+    return render(request, template_name, context)
+
 
 def restaurant_listview(request):
     template_name = 'restaurants/restaurantlocation_list.html'
@@ -31,10 +48,11 @@ class RestaurantListView(ListView):
 class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all()
 
+'''
     def get_object(self, *args, **kwargs):
         rest_id = self.kwargs.get('rest_id')
         obj = get_object_or_404(RestaurantLocation, id=rest_id)
         return obj
 
-
+'''
 
